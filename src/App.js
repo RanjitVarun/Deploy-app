9fab0d519f2263a9ea8f00f4532f31841ea0756e
RanjitVarun <varun98unplugged@gmail.com>
2022-08-09 15:30:45 +0530

    warning fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-//import "./App.css";
-// import {useState, useEffect} from 'react';
-// import axios from 'axios';
-import { List } from "./list";
-
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
 function App() {
   return (
     <div className="App">
       <h1>App live</h1>
-      {/* <List /> */}
     </div>
   );
 }
